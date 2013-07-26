ae008d73c80833b3dd5d5b87e83b2b4be30c59cb
roman semenov <r.v.semenov@gmail.com>
2013-07-26 16:49:24 +0700

    created project

---
+++ SydneyShark/SydneyShark/Classes/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  SydneyShark
+//
+//  Created by Roman on 7/26/13.
+//  Copyright Roman 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ SydneyShark/SydneyShark/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SydneyShark
+//
+//  Created by Roman on 7/26/13.
+//  Copyright Roman 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
